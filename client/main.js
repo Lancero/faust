f1dab2981ada24f935fad4c4f41782824f04759a
Piotr <sentinel.piotr@gmail.com>
2018-06-20 17:13:39 +0200

    restyling app

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+$('#year').text(new Date().getFullYear());
+
 
 
 
--- client/templates/userList/userList.js
+++ client/templates/userList/userList.js
@@ -1,11 +1,18 @@
 Template.userList.helpers({
-    appusers: function(){
+    appusers: function(){      
       return AppUsers.find();
     },
-    group: ()=>{
+    /*group: ()=>{
       return AppGroups.find();
-    }    
+    },*/
+    id: function(){
+      return this._id;  
+    },
+    group: function(){
+      return AppGroups.findOne({users: this._id});  
+    }
 });
+
 
 
 Template.userList.events({
